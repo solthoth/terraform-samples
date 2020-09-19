625f6a9ef0b202fa2306a19fb7740d1d2cc60964
Carlos Barajas <soldotsol@gmail.com>
2020-09-18 23:44:27 -0700

    docker sample

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # terraform-samples
+
+Using hyper-v provider: `git@github.com:taliesins/terraform-provider-hyperv.git`
--- docker-nginx.tf
+++ docker-nginx.tf
@@ -1,17 +1,17 @@
 provider "docker" {
-    host = "npipe:////.//pipe/docker_engine"
+  host = "npipe:////.//pipe/docker_engine"
 }
 
 resource "docker_image" "nginx" {
-    name = "nginx:latest"
-    keep_locally = false
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
 resource "docker_container" "nginx" {
-    image = docker_image.nginx.latest
-    name = "tutorial"
-    ports {
-        internal = 80
-        external = 8880
-    }
+  image = docker_image.nginx.latest
+  name  = "nginx-tutorial"
+  ports {
+    internal = 80
+    external = 8880
+  }
 }
